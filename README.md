6e9045e9f667d304555820f1406aadfb57481327
Charlie <30829462+Charlie-Hill@users.noreply.github.com>
2022-04-03 12:21:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Vue 3 + Typescript + Vite 2 + Tailwindcss + Electron 13 + Elctron-builder with preconfiged sample windows app
+
+![Project Status](https://github.com/CGQAQ/electron-vite-vue3/actions/workflows/build.yml/badge.svg)
+
+# Required Enviroments
+1. Nodejs >= 14.0.0
+2. MSVC Toolchain 2015 recommended
+3. Yarn 1.x
+
+# Run steps
+1. yarn --frozen-lockfile
+2. yarn start
+
+# Build steps
+1. yarn --frozen-lockfile
+2. yarn build
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img
+    class="m-auto bg-[#000]"
+    alt="Vue logo"
+    src="./assets/logo.png"
+  />
+  <HelloWorld
+    class=""
+    msg="Hello Vue 3 + TypeScript + Vite"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import HelloWorld from "./components/HelloWorld.vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style src="./tailwind-loader.css"></style>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
